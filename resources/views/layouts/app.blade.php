96f9f5b63eb80c949818857c4fe1a2d23a23b004
Rajesh-Dehury <56267680+Rajesh-Dehury@users.noreply.github.com>
2023-11-30 13:28:55 +0530

    default title change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
-    <title>@yield('title','Cohort Dashboard')</title>
+    <title>@yield('title','Dashboard')</title>
 
     {{-- daterange picker --}}
     <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
